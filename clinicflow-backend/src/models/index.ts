d7470c825131175398253d0986579e29b40e69d0
rkumbhare12345 <rkumbhare12345@gmail.commkdir myprojectgit config --global user.email rkumbhare12345@gmail.com>
2026-02-06 08:59:22 +0530

    new folder

--- clinicflow-backend/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export { User, IUser } from './User';
-export { Patient, IPatient } from './Patient';
-export { Doctor, IDoctor } from './Doctor';
-export { Appointment, IAppointment } from './Appointment';
-export { Hospital, IHospital } from './Hospital';
--- clinicflow-backend/src/types/xss-clean.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'xss-clean' {
-    import { RequestHandler } from 'express';
-
-    function xssClean(): RequestHandler;
-
-    export default xssClean;
-}
